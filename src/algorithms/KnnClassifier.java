8d451982858adde2a0d71ebba958ac974fd13fe5
NGauville <noreo.ercoli@gmail.com>
2018-01-29 16:49:39 +0100

    move kNN code to KnnClassifier

--- src/algorithms/KnnClassifier.java
+++ src/algorithms/KnnClassifier.java
@@ -1,5 +1,7 @@
 package algorithms;
 
+import java.util.ArrayList;
+import java.util.PriorityQueue;
 import data.*;
 
 public class KnnClassifier implements Classifier {
@@ -10,7 +12,49 @@ public class KnnClassifier implements Classifier {
 	}
 
 	public String getConsensus(Data data, Element element) {
-		Iris irisData = (Iris) data;
-		return irisData.getConsensus(element, K);
+		ArrayList<Element> list = getKNN(data, element, K);
+        ArrayList<LabelScore> labels = new ArrayList<>();
+
+        for (Element el : list) {
+            LabelScore cls = null;
+
+            for (LabelScore ls : labels) {
+                if (ls.getLabel().equals(el.getLabel())) {
+                    cls = ls;
+                    break;
+                }
+            }
+
+            if (cls == null) {
+                cls = new LabelScore(el.getLabel());
+                labels.add(cls);
+            }
+
+            cls.increment();
+        }
+
+        LabelScore max = labels.get(0);
+        for (LabelScore ls : labels) {
+            if (ls.getScore() > max.getScore()) {
+                max = ls;
+            }
+        }
+
+        return max.getLabel();
 	}
+
+    private ArrayList<Element> getKNN(Data data, Element e, int k) {
+        ArrayList<Element> knn = new ArrayList<>();
+        PriorityQueue<ElementDistance> q = new PriorityQueue<>();
+
+        for (int i = 0; i < data.size(); i++) {
+        	q.add(new ElementDistance(data.getElement(i), data.getElement(i).getManhattanDistance(e)));
+        }
+
+        for (int i = 0; i < Math.min(q.size(), k); i++) {
+            knn.add(q.poll().getElement());
+        }
+
+        return knn;
+    }
 }
